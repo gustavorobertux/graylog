6bd43214b7b49057e113ddf7ee2e495cd0621a56
Gustavo Roberto Rodrigues Gonçalves <38466612+gustavorobertux@users.noreply.github.com>
2021-12-01 17:53:48 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install apt-transport-https openjdk-8-jre-headless uuid-runtime pwgen
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+sudo apt-get update && sudo apt-get upgrade -y 
+sudo apt-get install apt-transport-https openjdk-8-jre-headless uuid-runtime pwgen -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 -y 
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-wget -qO- 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9DA31620334BD75D9DCB49F368818C72E52529D4' | sudo apt-key add -
+sudo apt-get update 
+sudo apt-get install mongodb-org -y
+wget -qO- 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9DA31620334BD75D9DCB49F368818C72E52529D4' | sudo apt-key add - -y
 sudo systemctl daemon-reload
 sudo systemctl enable mongod.service
 sudo systemctl restart mongod.service
@@ -12,7 +12,7 @@ sudo systemctl --type=service --state=active | grep mongod
 wget -q https://artifacts.elastic.co/GPG-KEY-elasticsearch -O myKey
 sudo apt-key add myKey
 echo "deb https://artifacts.elastic.co/packages/oss-7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt-get update && sudo apt-get install elasticsearch-oss
+sudo apt-get update && sudo apt-get install elasticsearch-oss -y
 sudo tee -a /etc/elasticsearch/elasticsearch.yml > /dev/null <<EOT
 cluster.name: graylog
 action.auto_create_index: false
@@ -22,9 +22,9 @@ sudo systemctl enable elasticsearch.service
 sudo systemctl restart elasticsearch.service
 sudo systemctl --type=service --state=active | grep elasticsearch
 wget https://packages.graylog2.org/repo/packages/graylog-4.2-repository_latest.deb
-sudo dpkg -i graylog-4.2-repository_latest.deb
-sudo apt-get update && sudo apt-get install graylog-server graylog-enterprise-plugins graylog-integrations-plugins graylog-enterprise-integrations-plugins
-echo -n "Enter Password: " && head -1 </dev/stdin | tr -d '\n' | sha256sum | cut -d" " -f1
+sudo yes | dpkg -i graylog-4.2-repository_latest.deb
+sudo apt-get update && sudo apt-get install graylog-server graylog-enterprise-plugins graylog-integrations-plugins graylog-enterprise-integrations-plugins -y
+echo -n "Defina uma senha para o graylog: " && head -1 </dev/stdin | tr -d '\n' | sha256sum | cut -d" " -f1
 sudo systemctl daemon-reload
 sudo systemctl enable graylog-server.service
 sudo systemctl start graylog-server.service
