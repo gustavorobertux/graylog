37d15208dee6389d89f2c62d6db2d0306c7b07bd
Gustavo Roberto Rodrigues Gonçalves <38466612+gustavorobertux@users.noreply.github.com>
2021-12-01 18:11:13 -0300

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/gustavorobertux/graylog/main/install.sh
+chmod +x install.sh
+sh install.sh && echo "Instalação concluida, acesse http://SEUIP:9000/ , exemplo http://10.0.0.1:9000/"
